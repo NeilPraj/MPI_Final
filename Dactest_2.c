a0791614aa6154828e857d7f30ba9de29eecc063
krutikoza <ozakrutik@gmail.com>
2025-04-20 15:53:14 -0400

    added button input

--- Dactest_2.c
+++ Dactest_2.c
@@ -34,7 +34,7 @@ bool MCP4725_setVoltage(i2c_inst_t * i2c_port, uint8_t i2c_addr, bool writeEEpro
 #define I2C_PORT i2c0
 #define I2C_SDA 16
 #define I2C_SCL 17
-#define DAC_ADDRESS 0x62 //Default address no jumpers, floating pins. 
+#define DAC_ADDRESS 0x62 //Default address no jumpers. 
 
 
 #define THRESHOLD_LO 2000
@@ -67,10 +67,19 @@ float measure_frequency_live_adc() {
     // Period in microseconds
     uint64_t period_us = t_end - t_start;
     if (period_us == 0) return 0;
-    printf("Period: %llu us\n", period_us);
+    // printf("Period: %llu us\n", period_us);
     // Convert to frequency
     float freq = 1e6f / (float)period_us;
     return freq;
+}
+
+float average_freq(float freq, uint16_t count){
+    uint32_t sum = 0; 
+    for(int i = 0; i < count; i++){
+        //printf("Frequency %d: %f\n", i, freq);
+        sum += freq; 
+    }
+    return sum/count;
 }
 
 
@@ -78,6 +87,9 @@ float measure_frequency_live_adc() {
 #define ERROR_TOLERANCE_HZ 1.0f
 #define MAX_DAC 4095
 #define DAC_STEP 10 // Small step for ramp tuning
+
+#define up_button 2
+#define down_button 3
 
 
 static float freq_buffer[AVG_COUNT];
@@ -102,9 +114,26 @@ int main()
 
     printf("I2C Initialised\n");
 
+
+    gpio_init(up_button);
+    gpio_init(down_button);
+    gpio_set_dir(up_button, GPIO_IN);
+    gpio_set_dir(down_button, GPIO_IN);
+    gpio_pull_up(up_button);
+    gpio_pull_up(down_button);
+
     uint16_t dac_Oct_values[] = {
-        493, 987, 1481, 1975, 2469, 2963, 3457, 3951
+        0,    // 0V
+        493,  // ~0.397V
+        987,  // ~0.794V
+        1481, // ~1.190V
+        1975, // ~1.587V
+        2469, // ~1.984V
+        2963, // ~2.381V
+        3457, // ~2.778V
+        4095  // 3.3V (full scale)
     };
+    
     
     float note_C_frequencies[] = {
         16.35,   // C0
@@ -119,38 +148,86 @@ int main()
     };
     
     float freq_buffer[AVG_COUNT];
+    float freq = 0; 
+
+
+    int count = 0;
+
+    bool pressed = false;
+    bool ok;
     
     while (true) {
-        for (int i = 0; i < 8; i++) {
+
+
+        
+       pressed = false;
+        printf("waiting for button input\n");
+        while(!pressed){
+            if(gpio_get(up_button) == 0){
+                printf("up button pressed\n");
+                sleep_ms(10);
+                count++;
+                if(count % 10 ==0){
+                    count = 0;
+                }
+                printf("Count: %d\n", count);
+                pressed = true;
+            } else if(gpio_get(down_button) == 0){
+                printf("down button pressed\n");
+                sleep_ms(10);
+                count--;
+                if(count < 0){
+                    count = 0;
+                }
+                printf("Count: %d\n", count);
+                pressed = true;
+            } else {
+                pressed = false;
+            }
+        }
+        sleep_ms(200);
+
+        ok = MCP4725_setVoltage(I2C_PORT, DAC_ADDRESS, false, dac_Oct_values[count], 1000 * 1000);
+            if (!ok) {
+                printf("Failed to set voltage\n");
+            } else {
+                printf("Voltage set to DAC value: %d and the frequency being referenced is %d\n", dac_Oct_values[count], (uint32_t)note_C_frequencies[count]);
+            }
+        float freqAvg;
+        freq = measure_frequency_live_adc();
+        freqAvg = average_freq(freq, AVG_COUNT);
+        printf("Average Frequency: %f Hz\n", freqAvg);
+        freqAvg = 0;
+        
+
+
+        
+       
+
+        /*
+        for (int i = 0; i < 9; i++) {
             bool ok = MCP4725_setVoltage(I2C_PORT, DAC_ADDRESS, false, dac_Oct_values[i], 1000 * 1000);
             if (!ok) {
                 printf("Failed to set voltage\n");
             } else {
-                printf("Voltage set to DAC value: %d\n", dac_Oct_values[i]);
+                printf("Voltage set to DAC value: %d and the frequency being referenced is %d\n", dac_Oct_values[i], (uint32_t)note_C_frequencies[i]);
             }
 
-            sleep_ms(2000);
             
-
+            
+            
             float freqAvg;
-            // Measure frequency 64 times at each DAC step
-            for (int j = 0; j < AVG_COUNT; j++) {
-                freq_buffer[j] = measure_frequency_live_adc();
-                freqAvg += freq_buffer[j];
-                printf("Measured Frequency: %u Hz\n", freq_buffer[j]);
-                sleep_ms(100);
-            }
-
-            freqAvg = freqAvg/AVG_COUNT;
+            freq = measure_frequency_live_adc();
+            freqAvg = average_freq(freq, AVG_COUNT);
             printf("Average Frequency: %f Hz\n", freqAvg);
             freqAvg = 0;
             sleep_ms(2000);
-
-            
-            
         }
+        */
+        
+        
+        
     }
 
     return 0;
 }
-
